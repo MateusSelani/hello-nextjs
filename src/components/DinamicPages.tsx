ee946e042a059d9ee367041917a0113f08400602
Mateus Selani <selaneitor@gmail.com>
2023-09-08 15:11:15 -0400

    atualizando dinamicpages

--- src/components/DinamicPages.tsx
+++ src/components/DinamicPages.tsx
@@ -1,9 +1,9 @@
 export default function DinamicPages({id, titulo, txt}: any) {
     return(
-        <div>
-            <h1>{titulo}</h1>
-            <p>{txt}</p>
-            <code>{id}</code>
+        <div className="m-2">
+            <h1 className="text-center text-5xl m-3">{titulo}</h1>
+            <h2 className="text-center text-gray-600">#{id}</h2>
+            <p className="mx-32">{txt}</p>
         </div>
     )
 }
