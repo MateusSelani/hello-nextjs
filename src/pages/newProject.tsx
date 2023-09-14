400a16624f13744ac3fc9fab6f12acccf60320da
Mateus Selani <selaneitor@gmail.com>
2023-09-13 20:30:43 -0400

    alteracoes no new project

--- src/pages/newProject.tsx
+++ src/pages/newProject.tsx
@@ -4,13 +4,18 @@ export default function NewProject() {
             <form action="" className="m-16 flex flex-col gap-2">
                 <h1 className="text-5xl text-center font-bold mb-10">New Project</h1>
                 <p className="m-2">Nome do Projeto:</p>
-                <input type="text" className="border border-blue-500 rounded-xl"/>
+                <input type="text" placeholder="Projeto:"
+                    className="border border-blue-500 rounded-xl"/>
                 <p className="m-2">Descricao do projeto:</p>
                 <textarea name="" id="" cols={30} rows={15}
-                    className="border border-blue-500 rounded-xl"/>
+                    className="border border-blue-500 rounded-xl"
+                    placeholder="Descrição:"
+                />
             </form>
                 <button className="flex flex-col mx-16 justify-start w-1/6
-                    bg-blue-800 text-white hover:bg-blue-900 rounded p-3">
+                    bg-blue-800 text-white hover:bg-blue-900 rounded p-3"
+                    type="submit"
+                >
                         Salvar!
                 </button>
         </div>
